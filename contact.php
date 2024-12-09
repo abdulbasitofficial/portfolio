ffd8d26ebce6369cc63da988a046796981c072d5
Abdul Basit Mangral <83729493+abdulbasitofficial@users.noreply.github.com>
2024-12-09 19:17:07 +0500

    php

---
+++ contact.php
@@ -0,0 +1,321 @@
+<!doctype html>
+<html lang="zxx">
+
+
+<!-- Mirrored from miller.bslthemes.com/arter-demo/contact.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 May 2024 17:46:07 GMT -->
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- color of address bar in mobile browser -->
+  <meta name="theme-color" content="#2B2B35">
+  <!-- favicon  -->
+  <link rel="shortcut icon" href="img/apple-touch-icon.png" type="image/x-icon">
+  <!-- bootstrap css -->
+  <link rel="stylesheet" href="css/plugins/bootstrap.min.css">
+  <!-- font awesome css -->
+  <link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+  <!-- swiper css -->
+  <link rel="stylesheet" href="css/plugins/swiper.min.css">
+  <!-- fancybox css -->
+  <link rel="stylesheet" href="css/plugins/fancybox.min.css">
+  <!-- main css -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Abdul Basit</title>
+</head>
+
+<body>
+
+  <!-- app -->
+  <div class="art-app">
+
+    <!-- mobile top bar -->
+    <div class="art-mobile-top-bar"></div>
+
+    <!-- app wrapper -->
+    <div class="art-app-wrapper">
+
+      <!-- app container end -->
+      <div class="art-app-container">
+
+        <!-- info bar -->
+        <?php include('bar.php'); ?>
+        <!-- info bar end -->
+
+        <!-- content -->
+        <div class="art-content">
+
+          <!-- curtain -->
+          <div class="art-curtain"></div>
+
+          <!-- top background -->
+          <div class="art-top-bg" style="background-image: url(img/bg.jpg)">
+            <!-- overlay -->
+            <div class="art-top-bg-overlay"></div>
+            <!-- overlay end -->
+          </div>
+          <!-- top background end -->
+
+
+          <!-- swup container -->
+          <div class="transition-fade" id="swup">
+
+            <!-- scroll frame -->
+            <div id="scrollbar" class="art-scroll-frame">
+
+              <!-- container -->
+              <div class="container-fluid">
+
+                <!-- row -->
+                <div class="row p-30-0">
+
+                  <!-- col -->
+                  <div class="col-lg-12">
+
+                    <!-- section title -->
+                    <div class="art-section-title">
+                      <!-- title frame -->
+                      <div class="art-title-frame">
+                        <!-- title -->
+                        <h4>Contact information</h4>
+                      </div>
+                      <!-- title frame end -->
+                    </div>
+                    <!-- section title end -->
+
+                  </div>
+                  <!-- col end -->
+                  <!-- col -->
+                  <div class="col-12 col-xl-6">
+                    <!-- contact card -->
+                    <div class="art-a art-card">
+                      <div class="art-table p-15-15">
+                        <ul>
+                          <li>
+                            <h6>Country:</h6><span>Pakistan</span>
+                          </li>
+                          <li>
+                            <h6>City:</h6><span>Islamabad</span>
+                          </li>
+
+                          <li>
+                            <h6>Streat:</h6><span>Shadaab Colony, Thandapani</span>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <!-- contact card end -->
+                  </div>
+                  <!-- col end -->
+                  <!-- col -->
+                  <div class="col-12 col-xl-6">
+                    <!-- contact card -->
+                    <div class="art-a art-card">
+                      <div class="art-table p-15-15">
+                        <ul>
+                          <li>
+                            <h6>Email:</h6><span>abdulbasitmangral@gmail.com</span>
+                          </li>
+                          <li>
+                            <h6>Contact:</h6><span>(+92) 302 8952654</span>
+                          </li>
+                          <li>
+                            <h6>Teams:</h6><span>abdulbasitmangral@gmail.com</span>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <!-- contact card end -->
+                  </div>
+                  <!-- col end -->
+
+                  <!-- col -->
+                  <div class="col-lg-12">
+
+                    <!-- section title -->
+                    <div class="art-section-title">
+                      <!-- title frame -->
+                      <div class="art-title-frame">
+                        <!-- title -->
+                        <h4>Get in touch</h4>
+                      </div>
+                      <!-- title frame end -->
+                    </div>
+                    <!-- section title end -->
+
+                    <!-- contact form frame -->
+                    <div class="art-a art-card">
+
+                      <!-- contact form -->
+                      <form id="form" action="forms/contact.php" class="art-contact-form">
+                        <!-- form field -->
+                        <div class="art-form-field">
+                          <!-- name input -->
+                          <input id="name" name="name" class="art-input" type="text" placeholder="Name" required>
+                          <!-- label -->
+                          <label for="name"><i class="fas fa-user"></i></label>
+                        </div>
+                        <!-- form field end -->
+                        <!-- form field -->
+                        <div class="art-form-field">
+                          <!-- email input -->
+                          <input id="email" name="email" class="art-input" type="email" placeholder="Email" required>
+                          <!-- label -->
+                          <label for="email"><i class="fas fa-at"></i></label>
+                        </div>
+                        <!-- form field end -->
+                        <!-- form field -->
+                        <div class="art-form-field">
+                          <!-- message textarea -->
+                          <textarea id="message" name="text" class="art-input" placeholder="Message"
+                            required></textarea>
+                          <!-- label -->
+                          <label for="message"><i class="far fa-envelope"></i></label>
+                        </div>
+                        <!-- form field end -->
+                        <!-- button -->
+                        <div class="art-submit-frame">
+                          <button class="art-btn art-btn-md art-submit" type="submit"><span>Send message</span></button>
+                          <!-- success -->
+                          <div class="art-success">Success <i class="fas fa-check"></i></div>
+                        </div>
+                      </form>
+                      <!-- contact form end -->
+
+                    </div>
+                    <!-- contact form frame end -->
+
+                  </div>
+                  <!-- col end -->
+
+                </div>
+                <!-- row end -->
+
+              </div>
+              <!-- container end -->
+
+              <!-- container -->
+              <div class="container-fluid">
+
+               <!-- footer -->
+               <?php include('footer.php'); ?>
+                <!-- footer end -->
+
+              </div>
+              <!-- container end -->
+
+            </div>
+            <!-- scroll frame end -->
+
+          </div>
+          <!-- swup container end -->
+
+        </div>
+        <!-- content end -->
+
+        <!-- menu bar -->
+        <div class="art-menu-bar">
+
+          <!-- menu bar frame -->
+          <div class="art-menu-bar-frame">
+
+            <!-- menu bar header -->
+            <div class="art-menu-bar-header">
+              <!-- menu bar button -->
+              <div class="art-menu-bar-btn">
+                <!-- icon -->
+                <span></span>
+              </div>
+              <!-- menu bar button end -->
+            </div>
+            <!-- menu bar header end -->
+
+            <!-- current page title -->
+            <div class="art-current-page"></div>
+            <!-- current page title end -->
+
+            <!-- scroll frame -->
+            <div class="art-scroll-frame">
+
+              <!-- menu -->
+              <nav id="swupMenu">
+                <!-- menu list -->
+                <ul class="main-menu">
+                  <!-- menu item -->
+                  <li class="menu-item"><a href="index.php">Home</a></li>
+                  <!-- menu item -->
+                  <li class="menu-item"><a href="portfolio.php">Portfolio</a></li>
+                  <!-- menu item -->
+                  <li class="menu-item"><a href="history.php">History</a></li>
+                  <!-- menu item -->
+                  <li class="menu-item current-menu-item" current-menu-item><a href="contact.php">Contact</a></li>
+                  <!-- menu item -->
+                  <li class="menu-item"><a href="blog.php">Blog</a></li>
+                </ul>
+                <!-- menu list end -->
+              </nav>
+              <!-- menu end -->
+
+            </div>
+            <!-- scroll frame end -->
+
+          </div>
+          <!-- menu bar frame -->
+
+        </div>
+        <!-- menu bar end -->
+
+      </div>
+      <!-- app container end -->
+
+    </div>
+    <!-- app wrapper end -->
+
+    <!-- preloader -->
+    <div class="art-preloader">
+      <!-- preloader content -->
+      <div class="art-preloader-content">
+        <!-- title -->
+        <h4>Abdul Basit</h4>
+        <!-- progressbar -->
+        <div id="preloader" class="art-preloader-load"></div>
+      </div>
+      <!-- preloader content end -->
+    </div>
+    <!-- preloader end -->
+
+  </div>
+  <!-- app end -->
+
+  <!-- jquery js -->
+  <script src="js/plugins/jquery.min.js"></script>
+  <!-- anime js -->
+  <script src="js/plugins/anime.min.js"></script>
+  <!-- swiper js -->
+  <script src="js/plugins/swiper.min.js"></script>
+  <!-- progressbar js -->
+  <script src="js/plugins/progressbar.min.js"></script>
+  <!-- smooth scrollbar js -->
+  <script src="js/plugins/smooth-scrollbar.min.js"></script>
+  <!-- overscroll js -->
+  <script src="js/plugins/overscroll.min.js"></script>
+  <!-- typing js -->
+  <script src="js/plugins/typing.min.js"></script>
+  <!-- isotope js -->
+  <script src="js/plugins/isotope.min.js"></script>
+  <!-- fancybox js -->
+  <script src="js/plugins/fancybox.min.js"></script>
+  <!-- swup js -->
+  <script src="js/plugins/swup.min.js"></script>
+
+  <!-- main js -->
+  <script src="js/main.js"></script>
+
+</body>
+
+
+<!-- Mirrored from miller.bslthemes.com/arter-demo/contact.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 May 2024 17:46:07 GMT -->
+
+</html>
