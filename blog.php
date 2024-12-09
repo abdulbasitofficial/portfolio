9f476d4843867fd75687f6a9fc2daa844c2e73f6
Abdul Basit Mangral <83729493+abdulbasitofficial@users.noreply.github.com>
2024-12-09 19:13:48 +0500

    php

---
+++ blog.php
@@ -0,0 +1,447 @@
+<!doctype html>
+<html lang="zxx">
+
+
+<!-- Mirrored from miller.bslthemes.com/arter-demo/portfolio-2-col.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 May 2024 17:46:14 GMT -->
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- color of address bar in mobile browser -->
+  <meta name="theme-color" content="#2B2B35">
+  <!-- favicon  -->
+  <link rel="shortcut icon" href="img/apple-touch-icon.png" type="image/x-icon">
+  <!-- bootstrap css -->
+  <link rel="stylesheet" href="css/plugins/bootstrap.min.css">
+  <!-- font awesome css -->
+  <link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+  <!-- swiper css -->
+  <link rel="stylesheet" href="css/plugins/swiper.min.css">
+  <!-- fancybox css -->
+  <link rel="stylesheet" href="css/plugins/fancybox.min.css">
+  <!-- main css -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Abdul Basit</title>
+</head>
+
+<body>
+
+  <!-- app -->
+  <div class="art-app">
+
+    <!-- mobile top bar -->
+    <div class="art-mobile-top-bar"></div>
+
+    <!-- app wrapper -->
+    <div class="art-app-wrapper">
+
+      <!-- app container end -->
+      <div class="art-app-container">
+
+        <!-- info bar -->
+        <?php include('bar.php'); ?>
+        <!-- info bar end -->
+
+        <!-- content -->
+        <div class="art-content">
+
+          <!-- curtain -->
+          <div class="art-curtain"></div>
+
+          <!-- top background -->
+          <div class="art-top-bg" style="background-image: url(img/bg.jpg)">
+            <!-- overlay -->
+            <div class="art-top-bg-overlay"></div>
+            <!-- overlay end -->
+          </div>
+          <!-- top background end -->
+
+
+          <!-- swup container -->
+          <div class="transition-fade" id="swup">
+
+            <!-- scroll frame -->
+            <div id="scrollbar" class="art-scroll-frame">
+
+
+              <!-- container -->
+              <div class="container-fluid">
+
+                <!-- row -->
+                <div class="row p-30-0">
+
+                  <!-- col -->
+                  <div class="col-lg-12">
+
+                    <!-- filter -->
+                    <div class="art-filter mb-30">
+                      <!-- filter link -->
+                      <p>My Blogs</p>
+                      <!-- filter link -->
+                    </div>
+                    <!-- filter end -->
+
+                  </div>
+                  <!-- col end -->
+
+                  <div class="art-grid art-grid-2-col art-gallery">
+
+                    <!-- grid item -->
+                    <div class="art-grid-item webTemplates">
+                      <!-- grid item frame -->
+                      <a data-fancybox="gallery" data-no-swup href="img/blog/1.jpg"
+                        class="art-a art-portfolio-item-frame art-horizontal">
+                        <!-- img -->
+                        <img src="img/blog/1.jpg" alt="item">
+                        <!-- zoom icon -->
+                        <span class="art-item-hover"><i class="fas fa-expand"></i></span>
+                      </a>
+                      <!-- grid item frame end -->
+                      <!-- description -->
+                      <div class="art-item-description">
+                        <!-- title -->
+                        <h5 class="mb-15">Blog</h5>
+                        <!-- button -->
+                      </div>
+                      <!-- description end -->
+
+                    </div>
+                    <!-- grid item end -->
+
+                    <!-- grid item -->
+                    <div class="art-grid-item logos">
+
+                      <!-- grid item frame -->
+                      <a data-fancybox="gallery" data-no-swup href="img/blog/2.jpg"
+                        class="art-a art-portfolio-item-frame art-horizontal">
+                        <!-- img -->
+                        <img src="img/blog/2.jpg" alt="item">
+                        <!-- zoom icon -->
+                        <span class="art-item-hover"><i class="fas fa-expand"></i></span>
+                      </a>
+                      <!-- grid item frame end -->
+                      <!-- description -->
+                      <div class="art-item-description">
+                        <!-- title -->
+                        <h5 class="mb-15">Blog</h5>
+                        <!-- button -->
+                      </div>
+                      <!-- description end -->
+
+                    </div>
+                    <!-- grid item end -->
+
+                    <!-- grid item -->
+                    <div class="art-grid-item drawings">
+
+                      <!-- grid item frame -->
+                      <a data-fancybox="gallery" data-no-swup href="img/blog/3.jpg"
+                        class="art-a art-portfolio-item-frame art-horizontal">
+                        <!-- img -->
+                        <img src="img/blog/3.jpg" alt="item">
+                        <!-- zoom icon -->
+                        <span class="art-item-hover"><i class="fas fa-expand"></i></span>
+                      </a>
+                      <!-- grid item frame end -->
+                      <!-- description -->
+                      <div class="art-item-description">
+                        <!-- title -->
+                        <h5 class="mb-15">Blog</h5>
+                        <!-- button -->
+                      </div>
+                      <!-- description end -->
+
+                    </div>
+                    <!-- grid item end -->
+
+                    <!-- grid item -->
+                    <div class="art-grid-item ui">
+
+                      <!-- grid item frame -->
+                      <a data-fancybox="gallery" data-no-swup href="img/blog/4.jpg"
+                        class="art-a art-portfolio-item-frame art-horizontal">
+                        <!-- img -->
+                        <img src="img/blog/4.jpg" alt="item">
+                        <!-- zoom icon -->
+                        <span class="art-item-hover"><i class="fas fa-expand"></i></span>
+                      </a>
+                      <!-- grid item frame end -->
+                      <!-- description -->
+                      <div class="art-item-description">
+                        <!-- title -->
+                        <h5 class="mb-15">Blog</h5>
+                        <!-- button -->
+                      </div>
+                      <!-- description end -->
+
+                    </div>
+                    <!-- grid item end -->
+
+                    <!-- grid item -->
+                    <div class="art-grid-item webTemplates">
+
+                      <!-- grid item frame -->
+                      <a data-fancybox="gallery" data-no-swup href="img/blog/5.jpg"
+                        class="art-a art-portfolio-item-frame art-horizontal">
+                        <!-- img -->
+                        <img src="img/blog/5.jpg" alt="item">
+                        <!-- zoom icon -->
+                        <span class="art-item-hover"><i class="fas fa-expand"></i></span>
+                      </a>
+                      <!-- grid item frame end -->
+                      <!-- description -->
+                      <div class="art-item-description">
+                        <!-- title -->
+                        <h5 class="mb-15">Blog</h5>
+                        <!-- button -->
+                      </div>
+                      <!-- description end -->
+
+                    </div>
+                    <!-- grid item end -->
+
+                    <!-- grid item -->
+                    <div class="art-grid-item logos">
+
+                      <!-- grid item frame -->
+                      <a data-fancybox="gallery" data-no-swup href="img/blog/6.jpg"
+                        class="art-a art-portfolio-item-frame art-horizontal">
+                        <!-- img -->
+                        <img src="img/blog/6.jpg" alt="item">
+                        <!-- zoom icon -->
+                        <span class="art-item-hover"><i class="fas fa-expand"></i></span>
+                      </a>
+                      <!-- grid item frame end -->
+                      <!-- description -->
+                      <div class="art-item-description">
+                        <!-- title -->
+                        <h5 class="mb-15">Blog</h5>
+                        <!-- button -->
+                      </div>
+                      <!-- description end -->
+                    </div>
+                    <!-- grid item end -->
+
+                    <!-- grid item -->
+                    <div class="art-grid-item drawings">
+
+                      <!-- grid item frame -->
+                      <a data-fancybox="gallery" data-no-swup href="img/blog/7.jpg"
+                        class="art-a art-portfolio-item-frame art-horizontal">
+                        <!-- img -->
+                        <img src="img/blog/7.jpg" alt="item">
+                        <!-- zoom icon -->
+                        <span class="art-item-hover"><i class="fas fa-expand"></i></span>
+                      </a>
+                      <!-- grid item frame end -->
+                      <!-- description -->
+                      <div class="art-item-description">
+                        <!-- title -->
+                        <h5 class="mb-15">Blog</h5>
+                        <!-- button -->
+                      </div>
+                      <!-- description end -->
+
+                    </div>
+                    <!-- grid item end -->
+
+                    <!-- grid item -->
+                    <div class="art-grid-item ui">
+
+                      <!-- grid item frame -->
+                      <a data-fancybox="gallery" data-no-swup href="img/blog/8.jpg"
+                        class="art-a art-portfolio-item-frame art-horizontal">
+                        <!-- img -->
+                        <img src="img/blog/8.jpg" alt="item">
+                        <!-- zoom icon -->
+                        <span class="art-item-hover"><i class="fas fa-expand"></i></span>
+                      </a>
+                      <!-- grid item frame end -->
+                      <!-- description -->
+                      <div class="art-item-description">
+                        <!-- title -->
+                        <h5 class="mb-15">Blog</h5>
+                        <!-- button -->
+                      </div>
+                      <!-- description end -->
+
+                    </div>
+                    <!-- grid item end -->
+                    <!-- grid item -->
+                    <div class="art-grid-item ui">
+
+                      <!-- grid item frame -->
+                      <a data-fancybox="gallery" data-no-swup href="img/blog/9.jpg"
+                        class="art-a art-portfolio-item-frame art-horizontal">
+                        <!-- img -->
+                        <img src="img/blog/9.jpg" alt="item">
+                        <!-- zoom icon -->
+                        <span class="art-item-hover"><i class="fas fa-expand"></i></span>
+                      </a>
+                      <!-- grid item frame end -->
+                      <!-- description -->
+                      <div class="art-item-description">
+                        <!-- title -->
+                        <h5 class="mb-15">Blog</h5>
+                        <!-- button -->
+                      </div>
+                      <!-- description end -->
+
+                    </div>
+                    <!-- grid item end -->
+                    <!-- grid item -->
+                    <div class="art-grid-item ui">
+
+                      <!-- grid item frame -->
+                      <a data-fancybox="gallery" data-no-swup href="img/blog/10.jpg"
+                        class="art-a art-portfolio-item-frame art-horizontal">
+                        <!-- img -->
+                        <img src="img/blog/10.jpg" alt="item">
+                        <!-- zoom icon -->
+                        <span class="art-item-hover"><i class="fas fa-expand"></i></span>
+                      </a>
+                      <!-- grid item frame end -->
+                      <!-- description -->
+                      <div class="art-item-description">
+                        <!-- title -->
+                        <h5 class="mb-15">Blog</h5>
+                        <!-- button -->
+                      </div>
+                      <!-- description end -->
+
+                    </div>
+                    <!-- grid item end -->
+
+                  </div>
+
+                </div>
+                <!-- row end -->
+
+              </div>
+              <!-- container end -->
+
+              <!-- container -->
+              <div class="container-fluid">
+
+                <!-- footer -->
+                <?php include('footer.php'); ?>
+                <!-- footer end -->
+
+              </div>
+              <!-- container end -->
+
+            </div>
+            <!-- scroll frame end -->
+
+          </div>
+          <!-- swup container end -->
+
+        </div>
+        <!-- content end -->
+
+        <!-- menu bar -->
+        <div class="art-menu-bar">
+
+          <!-- menu bar frame -->
+          <div class="art-menu-bar-frame">
+
+            <!-- menu bar header -->
+            <div class="art-menu-bar-header">
+              <!-- menu bar button -->
+              <div class="art-menu-bar-btn">
+                <!-- icon -->
+                <span></span>
+              </div>
+              <!-- menu bar button end -->
+            </div>
+            <!-- menu bar header end -->
+
+            <!-- current page title -->
+            <div class="art-current-page"></div>
+            <!-- current page title end -->
+
+            <!-- scroll frame -->
+            <div class="art-scroll-frame">
+
+              <!-- menu -->
+              <nav id="swupMenu">
+                <!-- menu list -->
+                <ul class="main-menu">
+                  <!-- menu item -->
+                  <li class="menu-item"><a href="index.php">Home</a></li>
+                  <!-- menu item -->
+                  <li class="menu-item "><a href="portfolio.php">Portfolio</a></li>
+                  <!-- menu item -->
+                  <li class="menu-item"><a href="history.php">History</a></li>
+                  <!-- menu item -->
+                  <li class="menu-item"><a href="contact.php">Contact</a></li>
+                  <!-- menu item -->
+                  <li class="menu-item current-menu-item"><a href="blog.php">Blog</a></li>
+                </ul>
+                <!-- menu list end -->
+              </nav>
+              <!-- menu end -->
+
+            </div>
+            <!-- scroll frame end -->
+
+          </div>
+          <!-- menu bar frame -->
+
+        </div>
+        <!-- menu bar end -->
+
+      </div>
+      <!-- app container end -->
+
+    </div>
+    <!-- app wrapper end -->
+
+    <!-- preloader -->
+    <div class="art-preloader">
+      <!-- preloader content -->
+      <div class="art-preloader-content">
+        <!-- title -->
+        <h4>Abdul Basit</h4>
+        <!-- progressbar -->
+        <div id="preloader" class="art-preloader-load"></div>
+      </div>
+      <!-- preloader content end -->
+    </div>
+    <!-- preloader end -->
+
+  </div>
+  <!-- app end -->
+
+  <!-- jquery js -->
+  <script src="js/plugins/jquery.min.js"></script>
+  <!-- anime js -->
+  <script src="js/plugins/anime.min.js"></script>
+  <!-- swiper js -->
+  <script src="js/plugins/swiper.min.js"></script>
+  <!-- progressbar js -->
+  <script src="js/plugins/progressbar.min.js"></script>
+  <!-- smooth scrollbar js -->
+  <script src="js/plugins/smooth-scrollbar.min.js"></script>
+  <!-- overscroll js -->
+  <script src="js/plugins/overscroll.min.js"></script>
+  <!-- typing js -->
+  <script src="js/plugins/typing.min.js"></script>
+  <!-- isotope js -->
+  <script src="js/plugins/isotope.min.js"></script>
+  <!-- fancybox js -->
+  <script src="js/plugins/fancybox.min.js"></script>
+  <!-- swup js -->
+  <script src="js/plugins/swup.min.js"></script>
+
+  <!-- main js -->
+  <script src="js/main.js"></script>
+
+</body>
+
+
+<!-- Mirrored from miller.bslthemes.com/arter-demo/portfolio-2-col.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 May 2024 17:46:14 GMT -->
+
+</html>
---
+++ footer.php
@@ -0,0 +1,39 @@
+<footer>
+                  <div class="row">
+                    <!-- col -->
+                    <div class="col-3 col-lg-3">
+                      <!-- brand -->
+                      <img class="art-brand" src="https://ptcl.com.pk/images/ptcl-logo-plain.svg" alt="brand"
+                        style="width: 100%;height: 100%;">
+                    </div>
+                    <!-- col end -->
+                    <!-- col -->
+                    <div class="col-3 col-lg-3">
+                      <!-- brand -->
+                      <img class="art-brand" src="https://www.f-cdn.com/assets/main/en/assets/freelancer-logo-light.svg"
+                        alt="brand" style="width: 100%;height: 100%;">
+                    </div>
+                    <!-- col end -->
+                    <!-- col -->
+                    <div class="col-3 col-lg-3">
+                      <!-- brand -->
+                      <img class="art-brand" src="https://appinsnap.com/images/resources/logo-light.png" alt="brand"
+                        style="width: 100%;margin-top: 15px;height: 50%;">
+                    </div>
+                    <!-- col end -->
+                    <!-- col -->
+                    <div class="col-3 col-lg-3">
+                      <!-- brand -->
+                      <img class="art-brand"
+                        src="https://wisemaninnovations.com/wp-content/uploads/2019/07/wiseman-logo-color.png"
+                        alt="brand" style="width: 100%;margin-top: 15px;height: 50%;">
+                    </div>
+                    <!-- col end -->
+                  </div>
+                  <!-- copyright -->
+                  <div class="art-copy" style="text-align: center;">©
+                    <span id="copyright">
+                      <script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))</script>
+                    </span> Abdul Basit
+                  </div>
+                </footer>
